c9587793b4d8cb6cfa644c28ed1cecaaccad3be8
MMiltonCM <MMiltonCM@192.168.1.5>
2018-04-10 22:47:43 -0500

    Modifique lo mio

--- OficinaBecasJava/src/Modelo/Becado.java
+++ OficinaBecasJava/src/Modelo/Becado.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
 /**
  *
- * @author Andre_Pando
+ * @author MMiltonCM
  */
 public class Becado {
+
+    private String nombreApoderado;
+    private String dirPadre;
+    private String telefPadre;
+    private String correoPadre;
+    private String expediente;
+    private String estadoDebe;
+
+    public Becado() {
+    }
+    
+    public String getNombreApoderado() {
+        return nombreApoderado;
+    }
+
+    public void setNombreApoderado(String nombreApoderado) {
+        this.nombreApoderado = nombreApoderado;
+    }
+
+    public String getDirPadre() {
+        return dirPadre;
+    }
+
+    public void setDirPadre(String dirPadre) {
+        this.dirPadre = dirPadre;
+    }
+
+    public String getTelefPadre() {
+        return telefPadre;
+    }
+
+    public void setTelefPadre(String telefPadre) {
+        this.telefPadre = telefPadre;
+    }
+
+    public String getCorreoPadre() {
+        return correoPadre;
+    }
+
+    public void setCorreoPadre(String correoPadre) {
+        this.correoPadre = correoPadre;
+    }
+
+    public String getExpediente() {
+        return expediente;
+    }
+
+    public void setExpediente(String expediente) {
+        this.expediente = expediente;
+    }
+
+    public String getEstadoDebe() {
+        return estadoDebe;
+    }
+
+    public void setEstadoDebe(String estadoDebe) {
+        this.estadoDebe = estadoDebe;
+    }
+    
     
 }
